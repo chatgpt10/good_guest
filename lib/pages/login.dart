6330864152a4c6a103784b320d4648b483f5c909
liuerjian <1913698873@qq.com>
2022-05-29 16:50:27 +0800

    feat:install Fluro

---
+++ lib/pages/login.dart
@@ -0,0 +1,18 @@
+// 登录页面
+// 引入核心库
+import 'package:flutter/material.dart';
+import 'package:good_guest/widgets/page_content.dart';
+
+// 创建无状态组件
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const PageContent(
+        name: '欢迎来到登录页',
+      ),
+    );
+  }
+}
